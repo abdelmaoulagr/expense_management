a217296f3102ac6c82933dc9ce7826a80494414d
Abdelmaoula El Guarai <abdelmola667@gmail.com>
2024-06-12 19:23:56 -0400

    Revert "update README file and routes"
    This reverts commit 1228850911129a0f1bf577db2922866a3a1fb8c9.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -77,7 +77,7 @@
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
 
                                     <!--dashboad -->
-                                    <a class="dropdown-item"  href="">
+                                    <a class="dropdown-item"  href="{{ route('dashboard') }}">
                                         Dashboard
                                     </a>
 
@@ -101,6 +101,7 @@
                                                     document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
+
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
